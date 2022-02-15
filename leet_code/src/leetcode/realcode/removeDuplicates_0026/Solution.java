d0f7de25c9e0e684c4351101c7dce9aa9950c655
张浩锐 <zhanghaorui@jczh56.com>
2022-02-15 09:31:36 +0800

    leetcode_0026 wrong answer init

---
+++ leet_code/src/leetcode/realcode/removeDuplicates_0026/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.realcode.removeDuplicates_0026;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author 张浩锐
+ * @version 1.0.0
+ * @ClassName Solution.java
+ * @Description TODO
+ * @createTime 2022年02月14日 16:50:00
+ */
+public class Solution {
+    private int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int k = 0;//[0,k]栈中元素
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] != nums[k]) {//放⼊栈
+                k++;
+            }
+        }
+        return k + 1;
+    }
+}
